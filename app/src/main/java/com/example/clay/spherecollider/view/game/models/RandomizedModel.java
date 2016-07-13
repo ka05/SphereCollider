5756b8f8e230d688ef042a9c71588959f8dd6484
Clay Herendeen <cherendeen@envative.com>
2016-07-13 13:22:03 -0400

    .gitignore is now working

--- app/src/main/java/com/example/clay/spherecollider/view/game/models/RandomizedModel.java
+++
@@ -1,9 +0,0 @@
-package com.example.clay.spherecollider.view.game.models;
-
-/**
- * Created by wyattmcbain on 5/15/15.
- */
-public interface RandomizedModel {
-    void setBounds(int x, int y);
-    int getSize();
-}
